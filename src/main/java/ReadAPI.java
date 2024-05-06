52cb16df64972258326cca9656d9b892c56f306a
MrArcano <salvatore-dario@hotmail.it>
2024-05-06 20:12:40 +0200

    [init]

--- src/main/java/ReadAPI.java
+++ src/main/java/ReadAPI.java
@@ -1,2 +1,51 @@
-package PACKAGE_NAME;public class ReadAPI {
+import com.google.gson.*;
+import org.apache.hc.client5.http.fluent.Request;
+import org.apache.hc.client5.http.fluent.Response;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReadAPI {
+    public static void main(String[] args) throws Exception {
+
+        String resultHttp = get("https://rickandmortyapi.com/api/character");
+
+        System.out.println(resultHttp);
+
+        // Creare un oggetto Gson
+        Gson gson = new Gson();
+
+        // Convertire il JSON in un oggetto JsonObject
+        JsonObject jsonObject = gson.fromJson(resultHttp, JsonObject.class);
+
+        JsonArray resultsArray = jsonObject.getAsJsonArray("results");
+
+        //System.out.println(resultsArray);
+
+        List<RickAndMortyCharacter> remList = new ArrayList<>();
+
+        for (JsonElement el : resultsArray){
+            String id = el.getAsJsonObject().getAsJsonPrimitive("id").getAsString();
+            String name = el.getAsJsonObject().getAsJsonPrimitive("name").getAsString();
+
+            remList.add(new RickAndMortyCharacter(Integer.parseInt(id),name));
+        }
+
+        System.out.println(remList);
+
+    }
+
+
+    public static String get(String url) {
+        String result = null;
+        try {
+            Response response = Request.get(url).execute();
+            result = response.returnContent().asString();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
 }
+
--- src/main/java/RickAndMortyCharacter.java
+++ src/main/java/RickAndMortyCharacter.java
@@ -1,2 +1,104 @@
-package PACKAGE_NAME;public class RickAndMortyCharacter {
+import com.google.gson.annotations.SerializedName;
+import java.util.List;
+
+public class RickAndMortyCharacter {
+    private int id;
+    private String name;
+    private String status;
+    private String species;
+    private String type;
+    private String gender;
+    private Origin origin;
+    private Location location;
+    private String image;
+    private List<String> episode;
+    private String url;
+    private String created;
+
+    @Override
+    public String toString() {
+        return "RickAndMortyCharacter{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    public RickAndMortyCharacter(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public Origin getOrigin() {
+        return origin;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public List<String> getEpisode() {
+        return episode;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getCreated() {
+        return created;
+    }
+
+    public static class Origin {
+        private String name;
+        private String url;
+
+        public String getName() {
+            return name;
+        }
+
+        public String getUrl() {
+            return url;
+        }
+    }
+
+    public static class Location {
+        private String name;
+        private String url;
+
+        public String getName() {
+            return name;
+        }
+
+        public String getUrl() {
+            return url;
+        }
+    }
 }
